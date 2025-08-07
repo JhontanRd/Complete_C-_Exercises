42986188c38defab4cee727a4f5966c54af9222f
Jhonatan Rodrigues <jhon.devbr@gmail.com>
2025-08-07 11:07:46 -0300

    Subindo ex de Log de Site com HashSet

---
+++ SysLogSite/LogSite/LogSite/Entities/LogRecord.cs
@@ -0,0 +1,29 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace LogSite.Entities
+{
+    class LogRecord
+    {
+        public string Username { get; set; }
+        public DateTime Instante { get; set; }
+
+        public override int GetHashCode()
+        {
+            return Username.GetHashCode();
+        }
+
+        public override bool Equals(object? obj)
+        {
+            if (!(obj is LogRecord))
+            {
+                return false;
+            }
+            LogRecord other = (LogRecord)obj;
+            return Username.Equals(other.Username);
+        }
+    }
+}
---
+++ SysLogSite/LogSite/LogSite/Program.cs
@@ -0,0 +1,40 @@
+﻿using System.IO;
+using LogSite.Entities;
+
+namespace LogSite
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("Enter with full path: ");
+            string directory = Console.ReadLine();
+            //string directory = @"C:\temp\in.txt";
+
+            HashSet<LogRecord> log = new HashSet<LogRecord>();
+            try
+            {
+                using (StreamReader sr = File.OpenText(directory))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] line = sr.ReadLine().Split(' ');
+                        string name = line[0];
+                        DateTime intant = DateTime.Parse(line[1]);
+
+                        log.Add(new LogRecord { Username = name, Instante = intant });
+                    }
+                }
+                int numbAccess = log.Count;
+                Console.WriteLine($"Total users: {numbAccess}");
+            }
+            catch (IOException ex)
+            {
+                Console.WriteLine(ex.Message);
+            }
+            
+
+
+        }
+    }
+}
