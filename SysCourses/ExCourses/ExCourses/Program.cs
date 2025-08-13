d8c47b391542fcb622b36dbd49db0959c954751e
Jhonatan Rodrigues <jhon.devbr@gmail.com>
2025-08-13 09:49:34 -0300

    Subindo exercicios de HashSet e Dictionary

---
+++ SysCourses/ExCourses/ExCourses/Program.cs
@@ -0,0 +1,50 @@
+﻿namespace ExCourses
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            HashSet<int> courseA = new HashSet<int>();
+            HashSet<int> courseB = new HashSet<int>();
+            HashSet<int> courseC = new HashSet<int>();
+
+            Console.Write("-> How many students for course A: ");
+            int quantityCourse = int.Parse(Console.ReadLine());
+            for(int i = 0; i < quantityCourse; i++)
+            {
+                Console.Write("-> Enter with student code: ");
+                int code = int.Parse(Console.ReadLine());
+                courseA.Add(code);
+            }
+            Console.WriteLine();
+
+            Console.Write("-> How many students for course B: ");
+            quantityCourse = int.Parse(Console.ReadLine());
+            for(int i = 0; i < quantityCourse; i++)
+            {
+                Console.Write("-> Enter with student code: ");
+                int code = int.Parse(Console.ReadLine());
+                courseB.Add(code);
+            }
+            Console.WriteLine();
+
+            Console.Write("-> How many students for course C: ");
+            quantityCourse = int.Parse(Console.ReadLine());
+            for(int i = 0; i < quantityCourse; i++)
+            {
+                Console.Write("-> Enter with student code: ");
+                int code = int.Parse(Console.ReadLine());
+                courseC.Add(code);
+            }
+            Console.WriteLine();
+
+            HashSet<int> students = new HashSet<int>(courseA);
+            students.UnionWith(courseB);
+            students.UnionWith(courseC);
+            int total = students.Count();
+
+            Console.Write($"-> Total students: {total}");
+
+        }
+    }
+}
---
+++ SysVotations/ExVotations/ExVotations/Program.cs
@@ -0,0 +1,46 @@
+﻿namespace ExVotations
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.Write("-> Enter file full path: ");
+            string path = Console.ReadLine();
+            //C:\temp\in.txt
+
+            Dictionary<string, int> candidates = new Dictionary<string, int>();
+            try
+            {
+                using(StreamReader sr = new StreamReader(path))
+                {
+                    while (!sr.EndOfStream)
+                    {
+                        string[] candidateData = sr.ReadLine().Split(',');
+                        string name = candidateData[0];
+                        int votes = int.Parse(candidateData[1]);
+
+                        if (candidates.ContainsKey(name))
+                        {
+                            candidates[name] += votes;
+                        }
+                        else
+                        {
+                            candidates.Add(name, votes);
+                        }
+                    }
+                }
+
+                Console.WriteLine();
+                foreach (KeyValuePair<string, int> item in candidates)
+                {
+                    Console.WriteLine($"-> {item.Key}: {item.Value}");
+                }
+            }
+            catch (IOException ex)
+            {
+
+                Console.WriteLine("-> [ERROR] FIle not found. " + ex.Message);
+            }
+        }
+    }
+}
