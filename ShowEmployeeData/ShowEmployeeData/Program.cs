d59ce5e2a6436312777962a9ea98b7640710b1e5
Jhonatan Rodrigues <jhon.devbr@gmail.com>
2025-10-06 20:57:58 -0300

    Subindo exercicios com lambda e lin

---
+++ ShowEmployeeData/ShowEmployeeData/Program.cs
@@ -0,0 +1,42 @@
+﻿using ShowEmployeeData.Entities;
+using System.Globalization;
+
+namespace ShowEmployeeData
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Excercise Employee\n");
+
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+
+            List<Employee> employees = new List<Employee>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] line = sr.ReadLine().Split(',');
+                    string name = line[0];
+                    string email = line[1];
+                    double salary = double.Parse(line[2], CultureInfo.InvariantCulture);
+                    employees.Add(new Employee(name, email, salary));
+                }
+            }
+
+            Console.Write("Enter salary: ");
+            double value = double.Parse(Console.ReadLine(), CultureInfo.InvariantCulture);
+
+            Console.WriteLine($"Email of people whose salary is more than {value.ToString("C2", new CultureInfo("pt-BR"))}:");
+
+            var userEmails = employees.Where(e => e.Salary > value).OrderBy(e => e.Email).Select(e => e.Email).ToList();
+
+            userEmails.ForEach(e => Console.WriteLine(e));
+
+            double total = employees.Where(e => e.Name[0] == 'M').Sum(e => e.Salary);
+            Console.WriteLine($"Sum os salary of people whose name starts with 'M': {total.ToString("C2", new CultureInfo("pt-BR"))}");
+        }
+    }
+}
---
+++ SysProductsPrices/SysProductsPrices/Program.cs
@@ -0,0 +1,41 @@
+﻿using System.Globalization;
+using System.Linq;
+using SysProductsPrices.Entities;
+
+namespace SysProductsPrices
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Exercise\n");
+
+            // C:\temp\in.csv
+            
+            Console.Write("Enter full file path: ");
+            string path = Console.ReadLine();
+            
+            
+            List<Product> products = new List<Product>();
+
+            using (StreamReader sr = File.OpenText(path))
+            {
+                while (!sr.EndOfStream)
+                {
+                    string[] data = sr.ReadLine().Split(',');
+                    string name = data[0];
+                    double price = double.Parse(data[1], CultureInfo.InvariantCulture);
+                    products.Add(new Product(name, price));
+                }
+            }
+
+
+            Console.WriteLine();
+            double averagePrice = products.Select(p => p.Price).DefaultIfEmpty(0.0).Average();
+
+            Console.Write($"Average price: {averagePrice.ToString("C2", new CultureInfo("pr-BR"))}\n");
+            List<string> productsName = products.Where(p => p.Price < averagePrice).OrderByDescending(p => p.Name).Select(p => p.Name).ToList();
+            productsName.ForEach(j => Console.WriteLine(j));
+        }
+    }
+}
